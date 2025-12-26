b1b41377a29ac10841004cc39d220efc1cc7fef7
YOUNESS ELBOUDALI <boudaliyounss@gmail.com>
2025-12-26 09:53:38 +0000

    addmodul1

---
+++ module_1/ex0/ft_garden_intro.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+if __name__ == "__main__":
+    print("=== Welcome to My Garden ===")
+    name = "Rose"
+    height = "25cm"
+    age = "30 days"
+    print(f"Plant: {name}\nHeight: {height}\nAge: {age}")
+    print()
+    print("=== End of Program ===")
---
+++ module_1/ex0/test.py
@@ -0,0 +1,3 @@
+#!/usr/bin/env python3
+from ft_garden_intro import *
+print("test")
